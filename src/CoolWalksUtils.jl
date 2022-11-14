3cd3362392426ead37e22b6bc2c79d64a3a64caf
Henrik Wolf <henrik-wolf@freenet.de>
2022-11-14 12:05:15 +0100

    added wsg84 crs to utils

--- src/CoolWalksUtils.jl
+++ src/CoolWalksUtils.jl
@@ -1,5 +1,19 @@
 module CoolWalksUtils
 
-# Write your package code here.
+    using ArchGDAL
 
+    """
+    Reference which holds the WSG84 (EPSG4326) ArchGDAL Spatial Reference System
+    with lon-lat order.
+    """
+    const OSM_ref = Ref{ArchGDAL.ISpatialRef}()
+
+    function __init__()
+        OSM_ref[] = ArchGDAL.importEPSG(4326; order=:trad)
+        nothing
+    end
+
+    export OSM_ref
+
+    
 end
