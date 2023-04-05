9f4c8d12e7fe42742378aa0cdef6b571aa6a9c33
Henrik Wolf <henrik-wolf@freenet.de>
2023-04-05 12:19:38 +0200

    projection and crs reinterps return input geom

--- src/Projection.jl
+++ src/Projection.jl
@@ -2,24 +2,26 @@
     project_local!(geo_array, center_lon, center_lat)
 
 projects iterable of `ArchGDAL.jl` geometries from the coordinate system of `first(geo_array)`
-to the `transverse mercator` projection centered at `center_lon`, `center_lat`.
+to the `transverse mercator` projection centered at `center_lon`, `center_lat`. Returns the projected `geo_array`.
 """
 function project_local!(geo_array, center_lon, center_lat)
     projstring = "+proj=tmerc +lon_0=$center_lon +lat_0=$center_lat"
     src = ArchGDAL.getspatialref(first(geo_array))
     dest = ArchGDAL.importPROJ4(projstring)
-    ArchGDAL.createcoordtrans(trans->project_geo_array!(geo_array, trans), src, dest)
+    ArchGDAL.createcoordtrans(trans -> project_geo_array!(geo_array, trans), src, dest)
+    return geo_array
 end
 
 """
     project_back!(geo_array)
 
 projects iterable of `ArchGDAL.jl` geometries from the coordinate system of `first(geo_array)`
-to the coordinate reference system given in `OSM_ref` (`EPSG4326`).
+to the coordinate reference system given in `OSM_ref` (`EPSG4326`). Returns the projected `geo_array`.
 """
 function project_back!(geo_array)
     src = ArchGDAL.getspatialref(first(geo_array))
-    ArchGDAL.createcoordtrans(trans->project_geo_array!(geo_array, trans), src, OSM_ref[])
+    ArchGDAL.createcoordtrans(trans -> project_geo_array!(geo_array, trans), src, OSM_ref[])
+    return geo_array
 end
 
 """
@@ -37,17 +39,18 @@ end
     reinterp_crs!(geom, crs)
 
 reinterprets the coordinates of `ArchGDAL.jl` geometry `geom` to be in the
-coordinate reference system `crs`.
+coordinate reference system `crs`. Returns the reinterpreted `geom`.
 """
 function reinterp_crs!(geom, crs)
     ArchGDAL.createcoordtrans(crs, crs) do trans
         ArchGDAL.transform!(geom, trans)
     end
+    return geom
 end
 
 """
     apply_wsg_84!(geom)
 
-reinterprets the coordinates of `ArchGDAL.jl` geometry `geom` to be in `OSM_ref` (`EPSG4326`).
+reinterprets the coordinates of `ArchGDAL.jl` geometry `geom` to be in `OSM_ref` (`EPSG4326`). Returns the reinterpreted `geom`.
 """
 apply_wsg_84!(geom) = reinterp_crs!(geom, OSM_ref[])
