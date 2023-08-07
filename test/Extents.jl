e8bf8bb63323d042b008af4e4ffcdb3b9a9482e3
Henrik Wolf <henrik-wolf@freenet.de>
2023-08-07 19:01:15 +0200

    tested extent stuff

--- test/Extents.jl
+++ test/Extents.jl
@@ -31,7 +31,7 @@ end
 end
 
 @testitem "Extent contains" begin
-    using Extents
+    using Extents, ArchGDAL
 
     e1 = Extent(X=(0.0, 2.0), Y=(0.0, 2.0))
     e2 = Extent(X=(1.0, 3.0), Y=(1.0, 3.0))
@@ -53,4 +53,36 @@ end
     @test extent_contains(e1, 1.0, 1.6)
     @test !extent_contains(e1, 20.0, 1.6)
     @test extent_contains(e1, 2.0, 1.0)
+
+
+    @test extent_contains(e1, ArchGDAL.createpoint(1.0, 1.4))
+    @test extent_contains(e1, ArchGDAL.buffer(ArchGDAL.createpoint(1.0, 1.0), 0.5))
+    @test !extent_contains(e1, ArchGDAL.buffer(ArchGDAL.createpoint(1.0, 1.0), 2.0))
+    @test !extent_contains(e1, ArchGDAL.buffer(ArchGDAL.createpoint(3.0, 1.0), 1.0))
+end
+
+@testitem "apply_extent!" begin
+    using ArchGDAL, Extents, DataFrames
+
+    e = Extent(X=(0.0, 2.0), Y=(0.0, 2.0))
+    lon = [1.0, 1.6, 2.6, 5.0]
+    lat = [1.0, 1.2, 3.5, 5.2]
+
+    p1 = ArchGDAL.createpoint(lon[1], lat[1])
+    p2 = ArchGDAL.buffer(ArchGDAL.createpoint(lon[2], lat[2]), 1.4)
+    p3 = ArchGDAL.createpoint(lon[3], lat[3])
+    p4 = ArchGDAL.buffer(ArchGDAL.createpoint(lon[4], lat[4]), 0.3)
+    geometry = [p1, p2, p3, p4]
+
+    df = DataFrame(lon=lon, lat=lat, geometry=geometry)
+    apply_extent!(df, e)
+    @test nrow(df) == 2
+    @test df.lon == [1.0, 1.6]
+    @test df.lat == [1.0, 1.2]
+
+    df = DataFrame(lon=lon, lat=lat, geometry=geometry)
+    apply_extent!(df, e; source=[:geometry])
+    @test nrow(df) == 1
+    @test df.lon == [1.0]
+    @test df.lat == [1.0]
 end
